136a17abf2c33bfd93e4a0321456cc8e46997fd1
hugoalh <32359235+hugoalh@users.noreply.github.com>
2020-07-01 11:48:29 +0800

    Update order

--- main.js
+++ main.js
@@ -6,5 +6,5 @@
 module.exports.version = "1.0.2";
 module.exports.flat = require("./flat.js");
 module.exports.merge = require("./merge.js");
-module.exports.reverse = require("./reverse.js");
 module.exports.removeDuplicate = require("./removeduplicate.js");
+module.exports.reverse = require("./reverse.js");
