28bcbce189ec51291c7cb55e5742fff33a731124
hugoalh <32359235+hugoalh@users.noreply.github.com>
2020-07-04 16:15:09 +0800

    Import alias

--- main.js
+++ main.js
@@ -3,8 +3,10 @@
 	Language:
 		NodeJS 14
 ==================*/
-module.exports.version = "1.0.4";
+const version = "1.0.5";
 module.exports.flat = require("./flat.js");
 module.exports.merge = require("./merge.js");
 module.exports.removeDuplicate = require("./removeduplicate.js");
 module.exports.reverse = require("./reverse.js");
+module.exports.v = version;
+module.exports.version = version;
