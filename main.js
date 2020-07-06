c366cfdec1243c554a693ae0cfc3344ed3b8f34a
hugoalh <32359235+hugoalh@users.noreply.github.com>
2020-07-06 17:24:14 +0800

    20200706

--- main.js
+++ main.js
@@ -3,10 +3,12 @@
 	Language:
 		NodeJS 14
 ==================*/
+const removeDuplicate = require("./removeduplicate.js");
 const version = "1.0.5";
 module.exports.flat = require("./flat.js");
 module.exports.merge = require("./merge.js");
-module.exports.removeDuplicate = require("./removeduplicate.js");
+module.exports.removeDuplicate = removeDuplicate;
 module.exports.reverse = require("./reverse.js");
+module.exports.unique = removeDuplicate;
 module.exports.v = version;
 module.exports.version = version;
