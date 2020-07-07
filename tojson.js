660aa56b04621af0afc511df7bb43afeff082e6b
hugoalh <32359235+hugoalh@users.noreply.github.com>
2020-07-07 11:23:55 +0800

    Improve function `toJSON`

--- tojson.js
+++ tojson.js
@@ -5,6 +5,17 @@
 ==================*/
 const advancedDetermine = require("@hugoalh/advanced-determine");
 const internalService = require("./internalservice.js");
+function toJSONInternal(item) {
+	let result = {};
+	item.forEach((value, index) => {
+		if (advancedDetermine.isArray(value) == true) {
+			result[index] = toJSONInternal(value);
+		} else {
+			result[index] = value;
+		};
+	});
+	return result;
+};
 /**
  * @function toJSON
  * @description Convert an array to JSON.
@@ -15,10 +26,6 @@ function toJSON(item) {
 	if (advancedDetermine.isArray(item) == false) {
 		return internalService.customTypeError(`Invalid type of "item"! Require type of array.`);
 	};
-	let result = {};
-	item.forEach((value, index) => {
-		result[index] = value;
-	});
-	return result;
+	return toJSONInternal(item);
 };
 module.exports = toJSON;
