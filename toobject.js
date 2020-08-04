750f4131e81003141a4346a7d0283c8c2ac5813b
hugoalh <32359235+hugoalh@users.noreply.github.com>
2020-08-04 11:32:39 +0800

    Rename function `toJSON` to `toObject`

--- toobject.js
+++ toobject.js
@@ -1,15 +1,15 @@
 /*==================
-[NodeJS] More Array - To JSON
+[NodeJS] More Array - To Object
 	Language:
 		NodeJS 14
 ==================*/
 const advancedDetermine = require("@hugoalh/advanced-determine");
 const internalService = require("./internalservice.js");
-function toJSONInternal(item) {
+function toObjectInternal(item) {
 	let result = {};
 	item.forEach((value, index) => {
 		if (advancedDetermine.isArray(value) == true) {
-			result[index] = toJSONInternal(value);
+			result[index] = toObjectInternal(value);
 		} else {
 			result[index] = value;
 		};
@@ -17,15 +17,15 @@ function toJSONInternal(item) {
 	return result;
 };
 /**
- * @function toJSON
- * @description Convert an array to JSON.
- * @param {array} item Array that need to convert to JSON.
- * @returns {object} A JSON with number-index key.
+ * @function toObject
+ * @description Convert an array to an object.
+ * @param {*[]} item Array that need to convert to object.
+ * @returns {object} An object with number-index key.
  */
-function toJSON(item) {
+function toObject(item) {
 	if (advancedDetermine.isArray(item) == false) {
 		return internalService.typeError(`Invalid type of "item"! Require type of array.`);
 	};
-	return toJSONInternal(item);
+	return toObjectInternal(item);
 };
-module.exports = toJSON;
+module.exports = toObject;
